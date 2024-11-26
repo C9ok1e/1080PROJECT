0f8d04465a1b4613aa586ab09b18175c7dceb52c
WataRui-39 <watanaberui37@gmail.com>
2024-11-27 01:35:13 +0800

    Update MatchAlgo.py

--- MatchAlgo.py
+++ MatchAlgo.py
@@ -4,18 +4,24 @@ import openpyxl
 #pip install pandas openpyxl
 #pip nstall openpyxl
 
+exit = bool(1)
 file = STUDENT_INFO.xlsx
-while true:
-           print("input 1 if sort by SUBJECT, 2 by LANGUAGE, 3 for BOTH")
+while exit == bool(1):
+           print("input 1 if sort by SUBJECT, 2 by LANGUAGE, 3 for BOTH and 4 to EXIT")
            sortby = input(int("Input filter of sort :")
-           if sortby == 1:
+           if sortby == 1 :
                       data = pd.read_excel(file)
-                      subdata = data[data['SUBJECT'] == SUBJECT]
+                      subdata = data[data['SUBJECT'] in subject]
                       print(subdata)
-           if sortby == 2:
+           elif sortby == 2 :
                       data = pd.read_excel(file)
-                      subdata = data[data['LANGUAGE'] == LANGUAGE]
+                      subdata = data[data['LANGUAGE'] in language]
                       print(subdata)
-                                 
-
-
+           elif sortby == 3 :
+                      data = pd.read_excel(file)
+                      subdata = data[data['LANGUAGE'] in language] and data[data['SUBJECT'] in subject]
+                      print(subdata)
+           elif sortby == 4 :
+                      exit == bool(0)
+           else :
+                      print('error! please try again!')
