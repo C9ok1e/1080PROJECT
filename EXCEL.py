92020435456fa2e08237f973b75545c2f1883d6f
Tinis777 <ltlikjlegg@gmail.com>
2024-11-26 23:38:16 +0800

    nigga

--- EXCEL.py
+++ EXCEL.py
@@ -16,7 +16,7 @@ data = {
 print("請輸入學生的資料，完成後輸入DONE結束。")
 
 while True:
-    CHECK = input("PRESS ANY BUTTON TO START OR TYPE 'END' TO LEAVE：")
+    CHECK = input("PRESS ANY BUTTON TO START OR TYPE 'DONE' TO LEAVE：")
     if CHECK.lower() == "done":
         break
     name = input("輸入 NAME：")
