b46ab19587813132bbd750001879074da8120aa4
WataRui-39 <watanaberui37@gmail.com>
2024-11-27 01:44:11 +0800

    Update MatchAlgo.py

--- MatchAlgo.py
+++ MatchAlgo.py
@@ -5,6 +5,7 @@ import openpyxl
 #pip nstall openpyxl
 
 exit = bool(1)
+#set exit code
 file = STUDENT_INFO.xlsx
 while exit == bool(1):
            print("input 1 if sort by SUBJECT, 2 by LANGUAGE, 3 for BOTH and 4 to EXIT")
@@ -12,10 +13,12 @@ while exit == bool(1):
            if sortby == 1 :
                       data = pd.read_excel(file)
                       subdata = data[data['SUBJECT'] in subject]
+                      # check if at least one subject is same
                       print(subdata)
            elif sortby == 2 :
                       data = pd.read_excel(file)
                       subdata = data[data['LANGUAGE'] in language]
+                      #check if at least one language is same
                       print(subdata)
            elif sortby == 3 :
                       data = pd.read_excel(file)
