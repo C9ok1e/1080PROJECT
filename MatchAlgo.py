594e24845c6bef5f8e37e41409912b90f4441ff1
WataRui-39 <watanaberui37@gmail.com>
2024-11-27 00:55:14 +0800

    Update MatchAlgo.py

--- MatchAlgo.py
+++ MatchAlgo.py
@@ -4,8 +4,9 @@ import openpyxl
 #pip install pandas openpyxl
 #pip nstall openpyxl
 
-sortby1 = input(str("Input first priority of sort :")
-sortby2 = input(str("Input second priority of sort :")
+print("input 1 if sort by subject, 2 by 
+sortby1 = input(int("Input first priority of sort :")
+sortby2 = input(int("Input second priority of sort :")
 file = STUDENT_INFO.xlsx
 read_excel(file,
 
